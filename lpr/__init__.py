e3ae774158b3b9f8c3dc11d92f7f9b88213c2d4e
Vijay Krishna <vjkrishna21@gmail.com>
2024-10-01 21:50:31 +0530

    initial commit

---
+++ lpr/__init__.py
@@ -0,0 +1,4 @@
+from .utils import process_license_plate
+
+__version__ = "0.1.0"
+__all__ = ["process_license_plate"]
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="lpr",
+    version="0.1.0",
+    author="VJ",
+    author_email="",
+    description="A library for license plate detection and recognition",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/lpr",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "pytesseract",
+        "easyocr",
+        "opencv-python",
+        "numpy",
+    ],
+)
