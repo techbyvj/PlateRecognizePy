2feda29739fcc4501a16268385bafda594095b0f
VJ <vjkrishna21@gmail.com>
2024-10-01 23:52:58 +0530

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="PlateRecognizePy",
-    version="0.1.1",
+    version="0.1.2",
     author="VJ",
     author_email="",
     description="A Python library for license plate detection and recognition",
